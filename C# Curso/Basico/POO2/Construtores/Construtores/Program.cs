0afc7b3a5bd49b85ae501e65ead678078aa936a9
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-01-16 13:14:34 -0400

    up

--- C# Curso/Basico/POO2/Construtores/Construtores/Program.cs
+++ C# Curso/Basico/POO2/Construtores/Construtores/Program.cs
@@ -2,3 +2,23 @@
 Console.WriteLine("Constructores \n");
 
 
+
+class Carro
+{
+
+    public Co
+
+
+
+    private int ruedas;
+    private double largo;
+    private double ancho;
+    private bool climatizador;
+    private string? tapiceria;
+
+
+
+
+}
+
+
