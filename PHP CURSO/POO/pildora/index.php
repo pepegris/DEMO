de1e6d43995e02e84658abff12ccf1233cd961ca
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-01-23 17:05:05 -0400

    up

--- PHP CURSO/POO/pildora/index.php
+++ PHP CURSO/POO/pildora/index.php
@@ -7,11 +7,51 @@ class Coche
     public $marca;
     public $estado;
 
+    function __construct($dato1,$dato2)
+    {
+        $this->ruedas=4; 
+        $this->color=$dato1;
+        $this->marca=$dato2;
+        $this->estado=false;
+
+        
+    } 
+
+    function info()
+    {
+        if ($this->estado==true) {
+            return "datos del carro $this->ruedas, $this->color, $this->marca, encendido <br> ";
+        }else {
+            return "datos del carro $this->ruedas, $this->color, $this->marca, apagado <br> ";
+        }
+    }
+
     function encender()
     {
-        return "arrancando";
+        $this->estado=true;
+        return "arrancando el carro <br>";
+    }
+    function apagar()
+    {
+        $this->estado=false;
+        return "apagando el carro <br>";
+    }
+    function acelerar()
+    {
+        return "acelerando el carro <br>";
+    }
+    function frenar()
+    {
+        return "frenando el carro <br>";
     }
 
     
 
 }
+
+$toyota= new Coche('azul','toyota');
+
+echo $toyota->info();
+echo $toyota->encender();
+echo $toyota->info();
+
