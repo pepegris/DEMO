b5b14c1a3099c71192580f0856ddd3a349f54c67
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2022-02-01 09:39:55 -0400

    upa

--- LARAVEL/laravel-trabajo/routes/web.php
+++ LARAVEL/laravel-trabajo/routes/web.php
@@ -14,12 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('nuevo');
-});
-
-
-Route::get('/nuevo', function () {
-
-    return view('nuevo');
-    
+    return view('welcome');
 });
